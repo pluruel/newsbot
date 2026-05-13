8661d804fb4fcad93163542fd652325321cb76c7
pluruel <pluruel@gmail.com>
2026-05-13 00:44:54 +0000

    fix(bots): export Trigger type from package

--- newsparser/bots/__init__.py
+++ newsparser/bots/__init__.py
@@ -1,3 +1,3 @@
-from newsparser.bots.core.types import Bot, Cron, TelegramMatch
+from newsparser.bots.core.types import Bot, Cron, TelegramMatch, Trigger
 
-__all__ = ["Bot", "Cron", "TelegramMatch"]
+__all__ = ["Bot", "Cron", "TelegramMatch", "Trigger"]
